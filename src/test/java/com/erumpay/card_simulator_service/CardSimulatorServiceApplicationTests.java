d26a4dc817ce56a4d22b14c8dcea853072108944
hajihyeok <jihyeok0502@gmail.com>
2026-05-13 17:22:29 +0900

    [KAN-1099] chore: setting

---
+++ src/test/java/com/erumpay/card_simulator_service/CardSimulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erumpay.card_simulator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardSimulatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
